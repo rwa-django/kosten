19f5e67def8f6b01b5105e5107ef197c6a95fb31
Remo <git@rwalser.ch>
2019-02-12 22:44:22 +0100

    Add model vehicles

--- vehicles/admin.py
+++ vehicles/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Vehicle_Fuel, Vehicle_Fuel_Pos, Vehicle_Type
+
+
+class VehicleTypeAdmin(admin.ModelAdmin):
+    list_display = ('login', 'type', 'aktiv', 'label')
+
+
+class VehicleFuelAdmin(admin.ModelAdmin):
+    list_display = ('login', 'type', 'info', 'booked',)
+    search_fields = ('login', 'type',)
+
+class VehicleFuelPosAdmin(admin.ModelAdmin):
+    list_display = ('fuel_id', 'pos', 'amount', 'km', 'liter', 'booked')
+
+admin.site.register(Vehicle_Type, VehicleTypeAdmin)
+admin.site.register(Vehicle_Fuel, VehicleFuelAdmin)
+admin.site.register(Vehicle_Fuel_Pos, VehicleFuelPosAdmin)
